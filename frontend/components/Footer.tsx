1c515a6fa730a9e1f1822bfc7c4b17356c4f1699
Ramandeep Bedi <ramansb100@gmail.com>
2024-06-11 01:50:27 +0100

    ensure footer doesn't cover bet button

--- frontend/components/Footer.tsx
+++ frontend/components/Footer.tsx
@@ -2,9 +2,10 @@
 import { faTwitter } from "@fortawesome/free-brands-svg-icons";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import GitHubIcon from '@mui/icons-material/GitHub';
+import styles from "./Footer.module.css";
 
 const Footer = () => {
-    return <div style={{ position: "fixed", bottom: 16, display: "flex", flexDirection: "row", justifyContent: "space-between", marginTop: 8, alignItems: "center", padding: 16, maxWidth: 660, width: "100%" }}>
+    return <div className={styles.footer}>
         <p style={{ flex: 1 }}>CoinFlip by 0xNascosta</p>
         <div style={{ display: "flex", alignItems: "center", gap: 12, position: "absolute", right: 16, }}>
             <a target="_blank" href="https://twitter.com/0xNascosta" rel="noopener noreferrer">
